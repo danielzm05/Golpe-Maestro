7980bccff38ae68628c535f3648b0f54cd77d741
Daniel Zerpa <danielzmfcbarcelona@gmail.com>
2024-01-02 15:35:05 -0300

    Improvement of the lives of combatants

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -142,7 +142,7 @@ class Fighter extends Sprite {
 
   takeHit() {
     takeHitSound.play()
-    this.health -= 20
+    this.health -= 5
 
     if (this.health <= 0) {
       this.switchSprites('death')
